45d0cc3d14b1e895fdc18f2e493df4bb377d1506
Matthias van Ingen <m.g.vaningen@students.uu.nl>
2020-01-07 13:26:34 +0100

    toevoegen routing

---
+++ manderijntje/manderijntje/Routing.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Way2Go
+{
+    interface IRouting
+    {
+        Route GetRoute(string startNode, string endNode, DataModel dataModel);
+    }
+
+    class Routing : IRouting
+    {
+        public int transfers;
+
+        public List<Node> GetShortestPathDijkstra(string startName, string endName, DateTime time, DataModel dataModel)
+        {
+            Node start = dataModel.GetNode(startName, dataModel.GetNodes());
+            Node end = dataModel.GetNode(endName, dataModel.GetNodes());
+            DijkstraSearch(start, end, time);
+            var shortestPath = new List<Node> {end};
+            BuildShortestPath(shortestPath, end);
+            shortestPath.Reverse();
+            return shortestPath;
+        }
+
+        private void BuildShortestPath(List<Node> list, Node node)
+        {
+            if (node.NearestToStart == null)
+                return;
+            list.Add(node.NearestToStart);
+            BuildShortestPath(list, node.NearestToStart);
+        }
+
+        private void DijkstraSearch(Node start, Node end, DateTime time)
+        {
+            start.MinCostToStart = 0;
+            var prioQueue = new List<Node> {start};
+            while (prioQueue.Any())
+            {
+                prioQueue = prioQueue.OrderBy(x => x.MinCostToStart).ToList();
+                var node = prioQueue.First();
+                prioQueue.Remove(node);
+                foreach (var link in node.Connecties.OrderBy(x => x.Weight))
+                {
+                    var childNode = link.End;
+                    if (childNode.Visited)
+                        continue;
+                    if (node.MinCostToStart + link.Weight < childNode.MinCostToStart)
+                    {
+                        childNode.MinCostToStart = node.MinCostToStart + link.Weight;
+                        childNode.NearestToStart = node;
+                        if (!prioQueue.Contains(childNode))
+                            prioQueue.Add(childNode);
+                    }
+                }
+
+                node.Visited = true;
+                if (node == end)
+                    return;
+            }
+        }
+
+        public Route GetRoute(string startName, string endName, DateTime Time, DataModel dataModel)
+        {
+            Route fastestRoute = new Route(startName, endName, Time, dataModel);
+            return fastestRoute;
+        }
+    }
+
+    class Route
+    {
+        public List<Node> shortestPath;
+        public DateTime startTime;
+        public DateTime endTime;
+        public int transfers;
+
+        public Route(string startName, string endName, DateTime time, DataModel dataModel)
+        {
+            Routing r = new Routing();
+            shortestPath = r.GetShortestPathDijkstra(startName, endName, time, dataModel);
+            startTime = time;
+            endTime = time.Add(TimeSpan.FromMinutes(shortestPath.Last().MinCostToStart));
+            transfers = r.transfers;
+
+        }
+
+        
+    }
+}
