5e5f38ff3fc75489645c5a8a067d42f7a5ca249b
Michael Bijker <michael@microcapital.nl>
2020-01-14 16:03:10 +0100

    intergratie vs

--- manderijntje/manderijntje/parsing.cs
+++ manderijntje/manderijntje/parsing.cs
@@ -401,6 +401,9 @@ namespace manderijntje
                 if (newNode.dummynode)
                 {
                     newNode.Color = Color.Orange;
+                } else
+                {
+                    newNode.dummynodeDrawLine = true;
                 }
 
                 dNodes.Add(newNode);
