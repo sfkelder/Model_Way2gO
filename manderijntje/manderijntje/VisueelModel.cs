e9dabcebc67f0b6f24039e9a54000005a96147c5
Michael Bijker <michael@microcapital.nl>
2020-01-15 14:41:38 +0100

    touche ups

--- manderijntje/manderijntje/VisueelModel.cs
+++ manderijntje/manderijntje/VisueelModel.cs
@@ -41,7 +41,7 @@ namespace manderijntje
 
         private void MakeDataForDisk(DataModel data)
         {
-            access = (new parsing(data, true)).getModel(false);
+            access = (new parsing(data)).getModel(false);
 
             if (Directory.Exists(filepath))
             {
